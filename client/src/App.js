c4cd568da048f4844ba8c8f939a523a47e004596
AKKHILESH R P <akkhileshrp@gmail.com>
2023-08-07 21:17:26 +0530

    Changes : Structure created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div>Hello, Developers!</div>
-  );
+    return (
+        <div className='App'>
+            <h1>ProTask: Empowering Productivity - Your Innovative To-Do Web App</h1>
+
+            <div className='todo-wrapper'>
+                <div className='todo-input'>
+                    <div className='todo-input-item'>
+                        <label>Title</label>
+                        <input type='text' placeholder='Enter the Work Assignment to be completed!' />
+                    </div>
+                    <div className='todo-input-item'>
+                        <label>Description</label>
+                        <input type='text' placeholder='Enter the Description of work.' />
+                    </div>
+                    <div className='todo-input-item'>
+                        <button type='button' className='primarybtn'>Add</button>
+                    </div>
+                </div>
+
+                <div className='btn-area'>
+                    <button>To-Do</button>
+                    <button>Completed</button>
+                </div>
+
+                <div className='todo-list'>
+                    <div className='todo-list-item'>
+                        <h3>Task 1</h3>
+                        <p>Description</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
